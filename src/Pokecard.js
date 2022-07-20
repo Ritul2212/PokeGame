8e661194827caf17e9d0f3ae413b8f03424b83cb
Ritul Koladiya <ritul.koladiya@gmail.com>
2022-07-20 13:11:38 +0530

    Initial commit

---
+++ src/Pokecard.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react';
+import './Pokecard.css'
+
+// const IMG_API='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const IMG_API=`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`;
+let padTOThree= (number) => (number <= 999 ? `00${number}`.slice(-3): number); 
+class Pokecard extends Component{
+    render(){
+        let imgSrc=`${IMG_API}${padTOThree(this.props.id)}.png`;
+        return(
+            <div className="Pokecard">
+                <h1 className="Pokecard-title">{this.props.name}</h1>
+                
+                <div className="Pokecard-img">
+                    <img src={imgSrc} alt={this.props.name}/>
+                </div>
+                <div className="Pokecard-data">Type:{this.props.type}</div>
+                <div className="Pokecard-data">Exp:{this.props.exp}</div>
+                
+            </div>
+        );
+    }
+}
+
+export default Pokecard;
